5243550c3153b17112b45fa5c5b397fb831cd092
redshoga <yui.ichioka.contact@gmail.com>
2019-11-03 21:31:42 +0900

    トップページから/readerに飛べるように

--- test/pages/index.spec.js
+++ test/pages/index.spec.js
@@ -1,14 +1,27 @@
-import { shallowMount } from '@vue/test-utils'
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import VueRouter from 'vue-router'
 import Index from '~/pages/index.vue'
 
 describe('pages/index', () => {
   let wrapper
+  const localVue = createLocalVue()
+  localVue.use(VueRouter)
+  const router = new VueRouter()
 
   beforeEach(() => {
-    wrapper = shallowMount(Index)
+    wrapper = shallowMount(Index, {
+      localVue,
+      router
+    })
   })
 
   test('Vue instance', () => {
     expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+
+  test('Jump to reader page', () => {
+    expect(wrapper.vm.$route.path).toBe('/')
+    wrapper.find('.button').trigger('click')
+    expect(wrapper.vm.$route.path).toBe('/reader')
   })
 })
