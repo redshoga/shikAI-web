1114d140294aa9eec342a178982c3bc342397b2d
redshoga <yui.ichioka.contact@gmail.com>
2019-11-04 21:14:13 +0900

    indexをスタイリング

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,6 +4,14 @@
   </div>
 </template>
 
+<style lang="scss">
+body {
+  background: linear-gradient(90deg, $color-main-1 0%, $color-main-2 100%);
+  color: $color-black;
+  @include text(normal, regular);
+}
+</style>
+
 <style lang="scss" scoped>
 .container {
   margin: 0 auto;
